97e8d81538d160a69662fc48ca16d62ece163c8c
Carlos Daniel <charly.lopez.perez@gmail.com>
2024-09-25 22:18:59 +0200

    feat: created basic routes

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ import { PORT } from "./config.js";
 
 const app = express();
 
-app.length("/", (req, res) => {
-  res.send("Hello World");
+// ### REGION: GET
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
 });
+
+app.get("/protected", (req, res) => {});
+
+// ### END REGION
+
+// ### REGION: POST
+
+app.post("/login", (req, res) => {});
+app.post("/register", (req, res) => {});
+app.post("/logout", (req, res) => {});
+
+// ### END REGION
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
